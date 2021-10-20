b99729c92526d01fc4f10c2f8fc9a4a081b4239a
George Mihov <gmihov001@gmail.com>
2021-10-20 19:25:40 +0000

    Updates

---
+++ src/front/js/pages/AddContact.js
@@ -0,0 +1,83 @@
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export const AddContact = () => {
+	
+    const { store, actions } = useContext(Context);
+	
+    const [contact, setContact] = useState({
+		name: null,
+		address: null,
+		phone: null,
+		email: null
+	});
+
+	const handleChange = e => {
+		setContact({ ...contact, [e.target.name]: e.target.value });
+	};
+
+	return (
+		<div className="container">
+			<div>
+				<h1 className="text-center mt-5">Add a new contact</h1>
+				<form>
+					<div className="form-group">
+						<label>Full Name</label>
+						<input
+							type="text"
+							name="name"
+							className="form-control"
+							placeholder="Full Name"
+							onChange={handleChange}
+						/>
+					</div>
+					<div className="form-group">
+						<label>Email</label>
+						<input
+							type="email"
+							name="email"
+							className="form-control"
+							placeholder="Enter email"
+							onChange={handleChange}
+						/>
+					</div>
+					<div className="form-group">
+						<label>Phone</label>
+						<input
+							type="phone"
+							name="phone"
+							className="form-control"
+							placeholder="Enter phone"
+							onChange={handleChange}
+						/>
+					</div>
+					<div className="form-group">
+						<label>Address</label>
+						<input
+							type="text"
+							name="address"
+							className="form-control"
+							placeholder="Enter address"
+							onChange={handleChange}
+						/>
+					</div>
+					<Link to="/">
+						<button
+							type="button"
+							className="btn btn-primary form-control"
+							disabled={!contact.name || !contact.address || !contact.phone || !contact.email}
+							onClick={() =>
+								actions.addContact(contact.name, contact.address, contact.phone, contact.email)
+							}>
+							SAVE
+						</button>
+					</Link>
+					<Link className="mt-3 w-100 text-center" to="/">
+						or get back to contacts
+					</Link>
+				</form>
+			</div>
+		</div>
+	);
+};
