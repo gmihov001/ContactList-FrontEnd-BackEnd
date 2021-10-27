2438d1ea2eeff4f08a391bc6e3f9484c70b599e2
George Mihov <gmihov001@gmail.com>
2021-10-27 15:03:57 +0000

    Completed jwt backend implementation

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,9 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+import datetime, json, os 
+
 
 api = Blueprint('api', __name__)
 
@@ -15,3 +18,18 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/login', methods=['POST'])
+def login():
+    credentials = request.json
+    email = credentials.get('email', None)
+    password = credentials.get('password', None)
+    user = User.query.filter_by(email=email, password=password).first()
+    
+    if user is None:
+        return jsonify({"msg": "Invalid email or password"}), 401
+
+    expires = datetime.timedelta(days=7)
+    access_token = create_access_token(identity=email, expires_delta=expires)
+
+    return jsonify({"token": access_token}), 200    
