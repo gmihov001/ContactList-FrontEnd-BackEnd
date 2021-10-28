6aded8b2677a4ccbc1dade45ae8bbf44311bee02
George Mihov <gmihov001@gmail.com>
2021-10-28 14:55:32 +0000

    Updates

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.email
@@ -24,15 +23,7 @@ class Contact(db.Model):
     full_name = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=False, nullable=True)
     phone = db.Column(db.Integer, unique=False, nullable=True)
-    address = db.Column(db.String(120), unique=False, nullable=True)
-
-    class Contact(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    full_name = db.Column(db.String(120), unique=False, nullable=False)
-    email = db.Column(db.String(120), unique=False, nullable=True)
-    phone = db.Column(db.Integer, unique=False, nullable=True)
-    address = db.Column(db.String(120), unique=False, nullable=True)
-    
+    address = db.Column(db.String(120), unique=False, nullable=True)    
 
     def __repr__(self):
         return "<Contact (full_name='%s', email='%s', phone='%s', address='%s')>" % (self.full_name, self.email, self.phone, self.address)
